92067dc52bbf1ea8bdf65b8ca98a5f1f6a362977
xiao-ml <1013404455@qq.com>
2020-08-11 16:00:09 +0800

    Update diy-part1.sh
    change

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,21 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# lede
+sed -i '$a src-git packages https://github.com/coolsnowwolf/packages' feeds.conf.default
+sed -i '$a src-git luci https://github.com/coolsnowwolf/luci' feeds.conf.default
+#sed -i '$a src-git routing https://git.openwrt.org/feed/routing.git' feeds.conf.default
+#sed -i '$a src-git telephony https://git.openwrt.org/feed/telephony.git' feeds.conf.default
+#sed -i '$a src-git freifunk https://github.com/freifunk/openwrt-packages.git' feeds.conf.default
+# lienol
+sed -i '$a src-git packages https://github.com/Lienol/openwrt-packages.git;dev-19.07' feeds.conf.default
+#sed -i '$a src-git routing https://git.openwrt.org/feed/routing.git;openwrt-19.07' feeds.conf.default
+#sed -i '$a src-git telephony https://git.openwrt.org/feed/telephony.git;openwrt-19.07' feeds.conf.default
+sed -i '$a src-git luci https://github.com/Lienol/openwrt-luci.git;dev-17.01' feeds.conf.default
+sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-package.git;master' feeds.conf.default
+# third
+sed -i '$a src-git routing git://github.com/openwrt-routing/packages.git' feeds.conf.default
+sed -i '$a src-git oui https://github.com/zhaojh329/oui.git' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git nas https://github.com/tobiaswaldvogel/openwrt-addpack.git' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
